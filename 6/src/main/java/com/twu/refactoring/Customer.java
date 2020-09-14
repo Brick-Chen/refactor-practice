befea8d993378f21e0cb82b2a2c53fce595cb003
YumingChen <mathcym@outlook.com>
2020-09-14 23:16:30 +0800

    [yuming chen] refactor 6.

--- 6/src/main/java/com/twu/refactoring/Customer.java
+++ 6/src/main/java/com/twu/refactoring/Customer.java
@@ -1,9 +1,21 @@
 package com.twu.refactoring;
 
+import org.omg.CORBA.PRIVATE_MEMBER;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Customer {
+	private static final int REGULAR_OFFSET = 2;
+	private static final double REGULAR_PLUS = 2;
+	private static final double CHILDREN_PLUS = 1.5;
+	private static final int CHILDREN_OFFSET = 3;
+	private static final double NEW_RELEASE_FACTOR = 3;
+	private static final double REGULAR_CHILDREN_FACTOR = 1.5;
+
+	private static final String STATEMENT_HEADER = "Rental Record for %s\n";
+	private static final String STATEMENT_FIGURE = "\t%s\t%.1f\n";
+	private static final String STATEMENT_FOOTER = "Amount owed is %.1f\nYou earned %d frequent renter points";
 
 	private String name;
 	private ArrayList<Rental> rentalList = new ArrayList<Rental>();
@@ -24,28 +36,13 @@ public class Customer {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		Iterator<Rental> rentals = rentalList.iterator();
-		String result = "Rental Record for " + getName() + "\n";
+		String result = String.format(STATEMENT_HEADER, getName());
 		while (rentals.hasNext()) {
 			double thisAmount = 0;
 			Rental each = rentals.next();
 
 			// determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-
-			}
+			thisAmount = calculateAmounts(each);
 
 			// add frequent renter points
 			frequentRenterPoints++;
@@ -55,16 +52,34 @@ public class Customer {
 				frequentRenterPoints++;
 
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
+			result += String.format(STATEMENT_FIGURE, each.getMovie().getTitle(), thisAmount);
 			totalAmount += thisAmount;
 
 		}
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
-				+ " frequent renter points";
+		result += String.format(STATEMENT_FOOTER, totalAmount, frequentRenterPoints);
 		return result;
+	}
+
+	private double calculateAmounts(Rental rental) {
+		double thisAmount = 0;
+		switch (rental.getMovie().getPriceCode()) {
+			case Movie.REGULAR:
+				thisAmount += REGULAR_PLUS;
+				if (rental.getDaysRented() > REGULAR_OFFSET)
+					thisAmount += (rental.getDaysRented() - REGULAR_OFFSET) * REGULAR_CHILDREN_FACTOR;
+				break;
+			case Movie.NEW_RELEASE:
+				thisAmount += rental.getDaysRented() * NEW_RELEASE_FACTOR;
+				break;
+			case Movie.CHILDRENS:
+				thisAmount += CHILDREN_PLUS;
+				if (rental.getDaysRented() > CHILDREN_OFFSET)
+					thisAmount += (rental.getDaysRented() - CHILDREN_OFFSET) * REGULAR_CHILDREN_FACTOR;
+				break;
+
+		}
+		return thisAmount;
 	}
 
 }
